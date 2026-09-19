933bdcb70d41d4948e14c7dd177bc482f73a31cb
cham-x <zakariyyahshamsudeen@gmail.com>
2026-09-19 10:51:28 +0100

    first commit

---
+++ app/subscriptions/[id].tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {useLocalSearchParams} from "expo-router";
+
+const SubscriptionDetails = () => {
+    const {id} = useLocalSearchParams<{id:string}>()
+    return (
+        <View>
+            <Text>Subscription Details: {id}</Text>
+        </View>
+    )
+}
+    export default SubscriptionDetails
